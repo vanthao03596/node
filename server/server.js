e8d3bbde21f3732f86c9c551677641af49aba707
thaopv <phamthao03596@gmail.com>
2019-01-16 15:40:38 +0700

    Update todo

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
+const _ = require('lodash')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -64,6 +65,28 @@ app.delete('/todos/:id', (req, res) => {
       return res.send({todo})
     })
     .catch(e => res.status(400).send())
+})
+
+app.patch('/todos/:id', (req, res) => {
+  const {id} = req.params
+  let body = _.pick(req.body, ['text', 'completed'])
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completed = new Date().getTime()
+  } else {
+    body.completed = false
+    body.completedAt = null
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then(todo => {
+      if (!todo) {
+        return res.status(404), send()
+      }
+      res.send({todo})
+    })
+    .catch(e => res.status(404).send())
 })
 
 app.listen(port, () => {
