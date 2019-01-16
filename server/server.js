776e3bae688a47e9f85449fe0c26c04884ddc4ee
thaopv <phamthao03596@gmail.com>
2019-01-16 14:14:25 +0700

    Add test for get todo by id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const {ObjectID} = require('mongodb')
 
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo')
@@ -31,6 +32,21 @@ app.get('/todos', (req, res) => {
       res.status(400).send(e)
     },
   )
+})
+
+app.get('/todos/:id', (req, res) => {
+  const {id} = req.params
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send()
+  }
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send()
+      }
+      return res.send({todo})
+    })
+    .catch(e => res.status(400).send())
 })
 app.listen(3000, () => {
   console.log('Stated on port 3000')
