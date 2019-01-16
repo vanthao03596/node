157ab6d44d6166a1e04c1d9cfc8e03c76c245325
thaopv <phamthao03596@gmail.com>
2019-01-16 15:47:47 +0700

    fix error mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,14 +7,15 @@ let opts = {
 
 let uri = process.env.DB_LOCALHOST
 
-if (process.env.APP_ENV && process.env.APP_ENV === 'production') {
+if (process.env.APP_ENV === 'production') {
   opts['auth'] = {
-    user: process.env.MONGO_DB_USER || '',
-    password: process.env.MONGO_DB_PASSWORD || '',
+    user: process.env.MONGO_DB_USER,
+    password: process.env.MONGO_DB_PASSWORD,
   }
   uri = process.env.MONGO_DB_URI
 }
 
+console.log(uri)
 mongoose.connect(
   uri,
   opts,
