4cda24425bf93a58e0847d4bab3d31d1699d989c
thaopv <phamthao03596@gmail.com>
2019-01-19 13:41:09 +0700

    Add cors

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const {ObjectID} = require('mongodb')
+const cors = require('cors')
 const _ = require('lodash')
 
 const {mongoose} = require('./db/mongoose')
@@ -12,6 +13,7 @@ const app = express()
 
 const port = process.env.PORT || 3000
 app.use(bodyParser.json())
+app.use(cors())
 app.post('/todos', (req, res) => {
   let todo = new Todo({
     text: req.body.text,
